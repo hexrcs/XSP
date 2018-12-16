80ae663f7138dba893a80d4d39dec0fad7796ef9
Xiaoru Li <dev@xiaoru.li>
2018-12-16 13:44:53 +0100

    Add some styles to Table

--- src/App/Results.js
+++ src/App/Results.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import styled from "@emotion/styled";
 import JsonTable from "ts-react-json-table";
 
 import store from "../store";
+
+const StyledTable = styled(JsonTable)`
+  margin-top: 2rem;
+  width: 45rem;
+  align-self: stretch;
+
+  .jsonOdd {
+    background: #eee;
+  }
+
+  thead {
+    /* text-align: center; */
+    border-bottom: 1px solid #ddd;
+  }
+`;
+
+const TableContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 45rem;
+`;
 
 @observer
 class Results extends Component {
   render() {
     return (
-      <div>
-        <JsonTable rows={store.result} />
+      <TableContainer>
+        <StyledTable rows={store.result} />
         {/* <pre>{JSON.stringify(store.result)}</pre> */}
-      </div>
+      </TableContainer>
     );
   }
 }
